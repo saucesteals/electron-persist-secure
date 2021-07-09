4a7258194fbb46d49b099866946521493197ed97
daniel <saucesteals@gmail.com>
2021-07-09 15:30:41 -0700

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { isNode } from "./utils/etc";
 import { createStoreBindings } from "./utils/bindings";
+import Store from "./Store";
 
-export default { Store: isNode ? require("./Store") : undefined, createStoreBindings };
+export default {
+  Store: isNode ? (require("./Store") as Store) : undefined,
+  createStoreBindings,
+};
