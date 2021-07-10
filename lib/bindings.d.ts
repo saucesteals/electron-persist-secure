3d2bde6985f86483d4d5047462e24f96afa93b59
daniel <saucesteals@gmail.com>
2021-07-09 21:26:55 -0700

    fix

--- lib/bindings.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const createStoreBindings: (storeName?: string) => {
-    get: (key: string, defaultValue?: any) => Promise<any>;
-    set: (key: string | object, value?: any) => Promise<any>;
-    has: (key: string) => Promise<boolean>;
-    reset: (...keys: string[]) => Promise<any>;
-    delete: (key: string) => Promise<any>;
-    clear: () => Promise<any>;
-};
--- lib/types.d.ts
+++
@@ -1,6 +0,0 @@
-export interface MessageReply {
-    success: boolean;
-    error?: string;
-    value?: any;
-}
-export declare type ConfigOperation = "get" | "set" | "has" | "reset" | "delete" | "clear";
